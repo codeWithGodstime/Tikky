7c544e4c86570bd7cf8c89e83987ecc7f83a304c
codeWithGodstime <specialgodstym@gmail.com>
2025-04-28 11:13:06 +0100

    fix production bug3

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import JoinGame, CreateGame
+from . import consumer
 
 urlpatterns = [
     path("create/", CreateGame.as_view()),
     path("join/<str:game_id>/", JoinGame.as_view())
+]
+
+websocket_urlpatterns = [
+    re_path(r"ws/(?P<username>\w+)/$", consumer.GameConsumer.as_asgi()),
 ]
--- backend/ticky/urls.py
+++ backend/ticky/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from game import consumer
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -8,6 +7,3 @@ urlpatterns = [
     path("", include("game.urls")),
 ]
 
-websocket_urlpatterns = [
-    re_path(r"ws/(?P<username>\w+)/$", consumer.GameConsumer.as_asgi()),
-]
